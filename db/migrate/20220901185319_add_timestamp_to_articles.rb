aa40ee5dbf06f328632a1e34d4ee20827aee2d5f
Fab <info@desane.com.uy>
2022-09-01 17:12:58 -0300

    Empezando con articulos

--- db/migrate/20220901185319_add_timestamp_to_articles.rb
+++ db/migrate/20220901185319_add_timestamp_to_articles.rb
@@ -1,4 +1,4 @@
-class AgregarColumnasArticulos < ActiveRecord::Migration[7.0]
+class AddTimestampToArticles < ActiveRecord::Migration[7.0]
   def change
     add_column :articles, :created_at, :datetime
     add_column :articles, :update_at, :datetime
