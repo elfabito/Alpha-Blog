c867e4bd31b40fc2bc25338dc8951972308f693c
Fab <info@desane.com.uy>
2022-09-05 10:48:51 -0300

    Los botones funcionan correctamente

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+     @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -20,5 +24,22 @@ class ArticlesController < ApplicationController
   else
     render 'new'
     end
+  end
+
+  def update
+@article = Article.find(params[:id])
+if @article.update(params.require(:article).permit(:title, :description))
+flash[:notice] = "Articulo actualizado correctamente"
+redirect_to @article
+else
+  render 'edit'
+end
+  end
+
+  def destroy
+@article = Article.find(params[:id])
+@article.destroy
+flash[:notice] = "Articulo borrado correctamente"
+redirect_to articles_path
   end
 end
