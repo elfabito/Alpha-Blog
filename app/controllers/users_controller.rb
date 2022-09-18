757b6991a673685fffc48234ebf9521735896c9e
Fab <info@desane.com.uy>
2022-09-18 18:00:15 -0300

    Pagination all views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
+  end
+
+  def index
+    @user = User.paginate(page: params[:page], per_page: 2)
+  end
+
   def new
     @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
-  end
-
-  def index
-    @user = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
   end
 
  def create
